288442f33f139ce3b535ea74d75b891e2c9f84cf
kokhnenko_vladimir <vladimir.kokhnenko@gmail.com>
2021-09-17 16:00:08 +0300

    реализовал работу по созданию фона, получению watermark

---
+++ BLL/ImageProvider.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace WorkWithImage.BLL
+{
+    public class ImageProvider : IDisposable
+    {
+        private readonly Font _font;
+        private readonly Bitmap _background;
+        private readonly Bitmap _watermark;
+        private readonly Bitmap _sourceImage;
+        private readonly string _textImage;
+        private readonly int _heightSourceImage;
+        private readonly int _widthSourceImage;
+        private readonly int _heightBackgroundImage;
+
+        public ImageProvider(
+            string textImage,
+            Bitmap watermark,
+            Bitmap sourceImage)
+        {
+            _watermark = watermark;
+            _sourceImage = sourceImage;
+            _heightSourceImage = _sourceImage.Height;
+            _widthSourceImage = _sourceImage.Width;
+            _heightBackgroundImage = _heightSourceImage + 100;
+
+            _font = new Font("Consolas", 20, System.Drawing.FontStyle.Regular);
+
+            _background = new Bitmap(_widthSourceImage, _heightBackgroundImage);
+            _textImage = textImage;
+        }
+
+        public void Dispose()
+        {
+            _font?.Dispose();
+            _background?.Dispose();
+        }
+
+        private void CreateBackground()
+        {
+            using (Graphics g = Graphics.FromImage(_background))
+            {
+                g.FillRectangle(
+                    System.Drawing.Brushes.Black, 1, 1, this._widthSourceImage - 2, this._heightBackgroundImage - 2);
+                g.DrawString(_textImage, _font, System.Drawing.Brushes.White, 10, 5);
+            }
+        }
+
+        public void SaveBG(string fileName, ImageFormat imgFormat)
+        {
+            _background.Save(fileName, imgFormat);
+        }
+
+        private void AddWaterMark(int startLeft, int startTop)
+        {
+            ColorMatrix color_matrix = new ColorMatrix();
+            color_matrix.Matrix33 = 0.5f;
+            using(ImageAttributes image_attributes = new ImageAttributes())
+            {
+                image_attributes.SetColorMatrices(color_matrix, null);
+
+                _watermark.MakeTransparent(_watermark.GetPixel(0, 0));
+
+                using (Graphics gr = Graphics.FromImage(this._sourceImage))
+                {
+                    Rectangle rect = new Rectangle(startLeft, startTop,
+                        _watermark.Width, _watermark.Height);
+                    gr.DrawImage(_watermark, rect, 0, 0,
+                        _watermark.Width, _watermark.Height,
+                        GraphicsUnit.Pixel, image_attributes);
+                }
+            }
+        }
+
+        private void AddMModifiedImageToBackground()
+        {
+
+        }
+
+        public void Work()
+        {
+            this.CreateBackground();
+            this.AddWaterMark(this._heightBackgroundImage - 50, 150);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Drawing.Imaging;
+using WorkWithImage.BLL;
+using System.Net;
 
 namespace WorkWithImage
 {
@@ -13,74 +16,26 @@ namespace WorkWithImage
     {
         static void Main(string[] args)
         {
+            string pathToWatermark = @"D:\MyWorkFolder\it\develop\avto.pro\Watermark\bin\Debug\site_logo.png";
+            string pathToSourceImage = @"https://abptest.blob.core.windows.net/images/c/Stellox_000006B-SX_1.jpg";
 
-            // параметры -->
-            //var imagePath = @"D:\input.jpg";
-            
-            string imagePath = "https://abptest.blob.core.windows.net/images/b/Stellox_000002BSX_1.jpg";
-            string text = "Это норма!";
-
-            var background = System.Windows.Media.Brushes.Black;
-            var textColor = System.Windows.Media.Brushes.White;
-
-            int gap = 20;
-            int fontSize = 70;
-
-            int dpi = 96;
-
-            var font =
-                new Typeface(
-                    new System.Windows.Media.FontFamily("Segoe UI"), FontStyles.Normal,
-                    FontWeights.Bold, FontStretches.SemiExpanded);
-            // <--
-
-            var image = BitmapFrame.Create(new Uri(imagePath));
-            double imageWidth = (double)image.PixelWidth;
-            double imageHeight = (double)image.PixelHeight;
-
-            var formattedText =
-                new FormattedText(
-                    text, CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
-                    font, fontSize, textColor, dpi)
-                {
-                    MaxTextWidth = imageWidth,
-                    TextAlignment = TextAlignment.Center
-                };
-
-            var textWidth = formattedText.Width;
-            var textHeight = formattedText.Height;
-
-            var totalWidth = (int)Math.Ceiling(imageWidth + 2 * gap);
-            var totalHeight = (int)Math.Ceiling(imageHeight + 3 * gap + textHeight);
-
-            var drawingVisual = new DrawingVisual();
-            using (var drawingContext = drawingVisual.RenderOpen())
+            using(Bitmap watermark = new Bitmap(pathToWatermark))
             {
-                drawingContext.DrawRectangle(
-                    background, null,
-                    new Rect(0, 0, totalWidth, totalHeight));
-
-                drawingContext.DrawImage(
-                    image,
-                    new Rect(gap, gap, imageWidth, imageHeight));
-                drawingContext.DrawText(
-                    formattedText,
-                    new System.Windows.Point(gap, imageHeight + 2 * gap));
+                using (WebClient client = new WebClient())
+                {
+                    using (Stream stream = client.OpenRead(pathToSourceImage))
+                    {
+                        using (Bitmap sourceImage = new Bitmap(stream))
+                        {
+                            using (ImageProvider imgProvider = new ImageProvider("FEBI 11138", watermark, sourceImage))
+                            {
+                                imgProvider.Work();
+                                imgProvider.SaveBG(@".\superTest.png", ImageFormat.Png);
+                            }
+                        }
+                    }
+                }
             }
-
-            var bmp =
-                new RenderTargetBitmap(
-                    totalWidth, totalHeight, dpi, dpi,
-                    PixelFormats.Pbgra32);
-            bmp.Render(drawingVisual);
-
-            var encoder = new JpegBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(bmp));
-            var resultPath = @"D:\output.jpg";
-            using (var stream = File.Create(resultPath))
-                encoder.Save(stream);
-
-            Console.ReadKey();
         }
     }
 }
